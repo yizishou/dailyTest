afb936a0b1a60bcbe6f9c5dcbe012daea08adfc3
yizhu.sun <yizhu.sun@tendcloud.com>
2016-12-08 19:09:53 +0800

    commit

--- src/main/java/cn/syz/common/JitTest.java
+++ src/main/java/cn/syz/common/JitTest.java
@@ -2,30 +2,21 @@ package cn.syz.common;
 
 public class JitTest {
   
-  private static int staticSum;
-
   private static int add(int a, int b) {
-    int aa = a;
-    int bb = b;
-    return aa + bb;
+    return a + b;
   }
   
   public static void main(String[] args) {
     int sum = 0;
-    for (int i = 0; i < 100000; i++) {
+    for (int i = 0; i < 1000000; i++) {
       int k = add(i, 1);
       sum += k;
     }
-    for (int i = 0; i < 100000; i++) {
+    for (int i = 0; i < 1000000; i++) {
       int k = i + 1;
       sum += k;
     }
-    for (int i = 0; i < 100000; i++) {
-      int k = i + 1;
-      JitTest.staticSum += k;
-    }
     System.out.println(sum);
-    System.out.println(JitTest.staticSum);
   }
 
 }
